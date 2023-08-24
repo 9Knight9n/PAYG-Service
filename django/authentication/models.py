29ffc912f56d38ffb4231ffeeff8f3f5b4abef3a
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2023-08-25 01:38:58 +0330

    Add record model to save API usage logs

--- django/authentication/models.py
+++ django/authentication/models.py
@@ -6,13 +6,6 @@ from django.contrib.auth.models import User as DjangoUser
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.OneToOneField(DjangoUser, on_delete=models.DO_NOTHING, null=False, blank=False)
-    is_manager = models.BooleanField(default=False, null=False, blank=False)
 
     def __str__(self):
         return self.user.username
-
-    def has_access_to_crm_panel(self):
-        return self.user.is_superuser or self.is_manager
-
-    def has_access_to_admin_panel(self):
-        return self.user.is_superuser
--- django/payg/models.py
+++ django/payg/models.py
@@ -1,3 +1,30 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models import F
+from authentication.models import User
 
-# Create your models here.
+
+class Record(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=False, blank=False)
+    api = models.CharField(max_length=127, null=False, blank=False)
+    month = models.PositiveSmallIntegerField(validators=[MaxValueValidator(12), MinValueValidator(1)],
+                                             null=False, blank=False)
+    year = models.PositiveSmallIntegerField(validators=[MaxValueValidator(2100), MinValueValidator(1950)],
+                                            null=False, blank=False)
+    count = models.PositiveIntegerField(default=0, null=False, blank=False)
+
+    class Meta:
+        unique_together = ['user', 'api', 'month', 'year']
+
+    @property
+    def date(self):
+        return f"{self.year}/{self.month}"
+
+    def __str__(self):
+        return f"{self.user}:{self.api}:{self.date} = {self.count}"
+
+    def increase_count(self):
+        self.count = F('count') + 1
+        self.save()
+
+
